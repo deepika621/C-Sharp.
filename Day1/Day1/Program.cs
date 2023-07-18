79bf5efc58a7680c8dd6842452782022d5f6e4d0
deepika621 <18311a12l2@sreenidhi.edu.in>
2023-07-18 16:21:03 +0530

    DAY 1

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Day1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=3; int b=4;
+            Console.WriteLine(a+b);
+        }
+    }
+}
