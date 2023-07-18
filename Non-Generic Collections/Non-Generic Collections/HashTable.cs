d6d43d56fabeaf5fb67a1193e0b17feef8223629
deepika621 <18311a12l2@sreenidhi.edu.in>
2023-07-19 00:25:22 +0530

    Commit

---
+++ Non-Generic Collections/Non-Generic Collections/HashTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Non_Generic_Collections
+{
+    public void HashTable(object sender, EventArgs e)
+    {
+        Hashtable ht = new Hashtable();
+        ht.Add("ora", "oracle");
+        ht.Add("vb", "vb.net");
+        ht.Add("cs", "cs.net");
+        ht.Add("asp", "asp.net");
+
+        foreach (DictionaryEntry d in ht)
+        {
+            Response.Write(d.Key + " " + d.Value);
+            Response.Write("<br>");
+        }
+    }
+}
---
+++ Non-Generic Collections/Non-Generic Collections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Non_Generic_Collections
+{
+    class Program
+    {
+        public void ArrayLists()
+        {
+            /*(ArrayList al = new ArrayList();
+            string str = "Kasarla Deepika";
+            int x = 7;
+            DateTime d = DateTime.Parse("21-jul-2000");
+            al.Add(str);
+            al.Add(x);
+            al.Add(d);
+
+            foreach (object o in al)
+            {
+                Console.WriteLine(o);
+                //Console.Write("<br>");
+            }*/
+            Hash.HashTable();
+        }
+    }
+}
