aae303af4d0846991a7019a3136d108284d23a61
deepika621 <18311a12l2@sreenidhi.edu.in>
2023-07-24 08:17:40 +0530

    Review

---
+++ Review2/Review2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Review2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello";
+            int count = 0;
+            foreach (var ch in str)
+            {
+                count++;
+            }
+            Console.WriteLine("Length of the string:"+ count);
+        }
+    }
+}
